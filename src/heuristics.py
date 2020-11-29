0f5e75f3dbcad152c8fb98df0bbd5fec23effb17
yquaro <yquaro@student.21-school.ru>
2020-11-29 16:06:45 +0300

    united heuristics function

--- src/heuristics.py
+++ src/heuristics.py
@@ -1,5 +1,21 @@
 import math
 
+def heuristic_decorator(func, result_state, size):
+    def heuristic_wrapper(puzzle):
+        return func(puzzle, result_state, size)
+
+    return heuristic_wrapper
+
+def uniform_cost(puzzle, result_state, size):
+    return 0
+
+def hamming_distance(puzzle, result_state, size):
+    res = 0
+    for i in range(size**2):
+        if puzzle[i] != 0 and puzzle[i] != result_state[i]:
+            res += 1
+    return res
+
 def manhattan(puzzle, result_state, size):
     res = 0
     for i in range(size*size):
@@ -10,3 +26,4 @@ def manhattan(puzzle, result_state, size):
             res += horizontal_shift + vertical_shift
     return res
 
+HEURISTICS = {'hamming': hamming_distance}
