c3f82f12ae7466698db6797615cec3e5bbed5adb
yquaro <yquaro@student.21-school.ru>
2020-12-05 14:26:25 +0300

    added linear_conflict heuristics func

--- src/heuristics.py
+++ src/heuristics.py
@@ -18,12 +18,48 @@ def hamming_distance(puzzle, result_state, size):
 
 def manhattan(puzzle, result_state, size):
     res = 0
-    for i in range(size*size):
+    for i in range(size**2):
         if puzzle[i] != 0 and puzzle[i] != result_state[i]:
             result_state_index = result_state.index(puzzle[i])
             horizontal_shift = abs(math.ceil(i / size) - math.ceil(result_state_index / size))
             vertical_shift = abs((i + size * horizontal_shift) - result_state_index)
             res += horizontal_shift + vertical_shift
     return res
+
+
+def result_state_matching(result_state, size):
+    rc = dict()
+    for i in range(size**2):
+        # key - result_state element
+        # value - [<element row>, <element column>, <element index>]
+        rc[result_state[i]] = [int(math.floor(i / size)), int(math.floor(i % size)), i]
+    return rc
+
+
+def linear_conflict(puzzle, result_state, size):
+    puzzle_matrix = [[puzzle[i + (j * size)] for i in range(size)] for j in range(size)]
+    rc = result_state_matching(result_state, size)
+    res = 0
+    for row in range(size):
+        for col in range(size - 1):
+            if col + 1 < size:
+                cur_res_state = rc[puzzle_matrix[row][col]]
+                next_res_state = rc[puzzle_matrix[row][col+1]]
+                if cur_res_state[0] == next_res_state[0] and cur_res_state[2] > next_res_state[2]:
+                    res += 2
+
+    for col in range(size):
+        for row in range(size - 1):
+            if row + 1 < size:
+                cur_res_state = rc[puzzle_matrix[row][col]]
+                next_res_state = rc[puzzle_matrix[row+1][col]]
+                if (cur_res_state[1] == next_res_state[1] and cur_res_state[2] > next_res_state[2]):
+                    res += 2
+    return res
+
+# puzzle = [1, 4, 3, 2, 5, 6, 12, 8, 9, 10, 11, 7, 13, 14, 15, 0]
+# size = 4
+# result_state = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0]
+# print(linear_conflict(puzzle, result_state, size))
 
 HEURISTICS = {'hamming': hamming_distance,'manhattan': manhattan}
--- src/result_state.py
+++ src/result_state.py
@@ -23,13 +23,13 @@ def zero_last_state(puzzle, size):
 
 
 def spiral_state(puzzle, size):
-    ans = [0 for _ in range(size*size)]
-    seen = [False for _ in range(size * size)]
+    ans = [0 for _ in range(size**2)]
+    seen = [False for _ in range(size**2)]
     angle_r = [0, 1, 0, -1]
     angle_c = [1, 0, -1, 0]
     counter = 1
     r = c = angle_i = 0
-    for _ in range(size*size):
+    for _ in range(size**2):
         ans[r * size + c] = counter
         seen[r * size + c] = True
         cr, cc = r + angle_r[angle_i], c + angle_c[angle_i]
@@ -38,7 +38,7 @@ def spiral_state(puzzle, size):
         else:
             angle_i = (angle_i + 1) % 4
             r, c = r + angle_r[angle_i], c + angle_c[angle_i]
-        counter = (counter + 1) % (size * size)
+        counter = (counter + 1) % (size**2)
     return tuple(ans)
 
 # puzzle 4x4
